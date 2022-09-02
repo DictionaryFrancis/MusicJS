b8ce7a7dc603866a955e2f8952502ebaa5dc7556
Francis Matias <fwmatias64@gmail.com>
2022-09-02 15:34:41 +0100

    Update Js

--- main/main.js
+++ main/main.js
@@ -1,9 +1,33 @@
-// let pom = document.querySelector(".tecla_pom")
+// You need to create a function onclick in the HTML
+//  function pom(){
+//      document.querySelector("#som_tecla_pom").play()
+//  }
 
+// let pom = document.querySelector(".tecla_pom")
 // pom.addEventListener("click", (e) =>{
-//     alert("POM")
+//     document.querySelector("#som_tecla_pom").play()
 // })
 
-// function pom(){
-//     alert("POM")
+// let clap = document.querySelector(".tecla_clap");
+// function playSoundclap() {
+//     document.querySelector("#som_tecla_clap").play();
 // }
+
+// clap.onclick = playSoundclap;
+
+const teclas = document.querySelectorAll(".tecla")
+console.log(teclas[2])
+
+
+function playSoundPom() {
+    document.querySelector("#som_tecla_pom").play();
+}
+
+let cont = 0;
+
+while(cont < teclas.length){
+    teclas[cont].onclick = playSoundPom;
+
+    cont += 1;
+
+}
